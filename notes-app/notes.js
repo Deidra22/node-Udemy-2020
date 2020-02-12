f4bac92c13e41477cc0b1633ad509ebfeccd491d
deidra <deidraann22@gmail.com>
2020-02-12 11:54:34 -0800

    add new notes function with if/else to check for duplicates

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,41 @@
+const fs = require('fs')
+
+
 const getNotes = function(){
     return 'Your notes...'
 }
 
 const addNote = function (title,body){
+    const notes = loadNotes()
+    const duplacteNotes = notes.filter(function (note) {
+        return note.title === title
+    })
 
+    if (duplacteNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New Note Added!')
+    } else {
+        console.log('Note title not available!')
+    }
+}
+
+const saveNotes = function (notes){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function (){
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
 }
 
 module.exports = {
