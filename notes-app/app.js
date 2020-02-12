71ee883a39443accb8502b45366234f89c3fcac4
deidra <deidraann22@gmail.com>
2020-02-12 11:29:49 -0800

    test commit

---
+++ notes-app/app.js
@@ -0,0 +1,59 @@
+// const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: function(){
+        console.log('Heres Your Notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'read the list',
+    handler: function(){
+        console.log('reading the list')
+    }
+})
+
+yargs.parse()
+// console.log(yargs.argv)
